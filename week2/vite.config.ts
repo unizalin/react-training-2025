69666510af189c13a80e9ed0d1c0601c4ed53300
unizalin <unizalin@gmail.com>
2025-01-06 22:57:44 +0800

    新增第二週 檔案環境建置

--- week2/vite.config.ts
+++ week2/vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production'? '/react-training-2025/': '/',
+  base: process.env.NODE_ENV === 'production' ? '/react-training-2025/' : '/',
   plugins: [react()],
 })
